d00cba2857b46762f81fed5b4918781134a6266f
Vinay-17 <vinayveerapaneni068@gmail.com>
2024-04-30 16:06:41 +0530

    vinay

---
+++ ps1.js
@@ -0,0 +1,24 @@
+n=10 
+i=1
+while(n>=i){
+    console.log(i)
+    i++
+}
+m=10
+i=m
+while(i>=1){
+    console.log(i)
+    i--
+}
+o=10
+i=10
+while(i>=1){
+    console.log(-i)
+    i--
+}
+p=10
+i=1
+while(p>=i){
+    console.log(-i)
+    i++
+}

55d30ed7a7c9d22bdc5c069dcb4cc6369c5238dd
Vinay-17 <vinayveerapaneni068@gmail.com>
2024-04-30 16:15:28 +0530

    vinay

--- ps2.js
+++ ps2.js
@@ -26,3 +26,33 @@ for(i=1;i<=prime.length;i++){
     }
 }
 console.log("biggest prime is:",max)
+
+num="5497351"
+prime="";
+for(n of num){
+    if(n>1){
+     a=true;
+for(i=2;i<n;i++){
+    if(n%i==0){
+        a=false
+        break
+    }
+}
+if(a)
+  prime=prime+n
+}
+}
+console.log(prime)
+max=Number(prime[0])
+min=Number(prime[0])
+for(i=1;i<=prime.length;i++){
+    if(prime[i]>max){
+        max=prime[i]
+    }
+    if(prime[i]<min){
+        min=prime[i]
+    }
+}
+console.log("maximum prime is",max)
+console.log(" minimum prime is",min)
+console.log(Number(max)+Number(min))
